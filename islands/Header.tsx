c8212be7c6bc60b7d84eb3a89878d0ce6f2508ca
theunholytim <helena@canyourwindowsdothat>
2023-09-19 20:48:55 -0300

    fixing

--- islands/Header.tsx
+++ islands/Header.tsx
@@ -5,26 +5,25 @@ type Props = {
   };
 
 const menus = [
- {name : "Home", href: "/"},
- {name : "Mugs", href: "/Mugs"},
+   {name : "Home", href: "/"},
+   {name : "Mugs", href: "/Mugs"},
+   {name: "About", href: "/About"},
 ];
 
 export default function Header( { active } : Props ) {
 
   return (
-     <div class="bg-white w-full max-w-screen-lg py-6 px-8 flex flex-col md:flex-row gap-4">
-       <div class="flex items-center flex-1">
-        <IconMug aria-hidden="true" />
-          <div class="text-2xl ml-1 font-bold">
-           to have at all times 
-         </div>
-      </div>
-      <ul class="flex items-left gap-2">
+     <div class="bg-transparent w-full max-w-screen-lg py-6 px-8 flex flex-col md:flex-row gap-4">
+       <ul class="flex items-left gap-2">
+         <li>
+             <IconMug aria-hidden="true" />
+          </li>
+
         {menus.map((menu) => (
           <li>
             <a
               href={menu.href}
-              class={"text-gray-500 hover:text-gray-700 py-1 border-gray-500" +
+              class={"text-white-500 hover:text-gray-700 py-1 border-gray-500" +
                 (menu.href === active ? " font-bold border-b-2" : "")}
             >
               {menu.name}
--- routes/index.tsx
+++ routes/index.tsx
@@ -4,10 +4,10 @@ import Header from "../islands/Header.tsx" ;
 
 export default function Home() {
   return (
-    <div class="px-4 py-8 mx-auto bg-[#ff2cc]">
+    <div class="px-4 py-8 mx-auto bg-[#620dbc]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <Header /> 
-        </div>
+       </div>
       </div>
   );
 }
